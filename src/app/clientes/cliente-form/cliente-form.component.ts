407f66e2e1c0921d80fc523989c8b5232558990d
Alexander Massuda <alexcaiom@gmail.com>
2020-04-12 01:10:13 -0300

    Funcoes de insercao, selecao e atualizacao parcial

--- src/app/clientes/cliente-form/cliente-form.component.ts
+++ src/app/clientes/cliente-form/cliente-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Cliente } from 'src/app/cliente/models/cliente';
+import { ClienteService } from 'src/app/cliente/services/cliente.service';
+import { DocumentReference } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-cliente-form',
@@ -9,25 +12,37 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ClienteFormComponent implements OnInit {
 
-  clienteForm: FormGroup;
+  clientForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    private service: ClienteService
     ) { }
 
   ngOnInit(): void {
-    this.clienteForm = this.formBuilder.group({
+    this.clientForm = this.formBuilder.group({
       nome: ['', Validators.required],
       endereco: ['', Validators.required],
       casado: false
     });
   }
 
-  salvarCliente() {
-    if(this.clienteForm.invalid) {
+  saveCliente() {
+    if(this.clientForm.invalid) {
       return;
     }
-    
+
+    let cliente: Cliente = this.clientForm.value;
+    cliente.modificacao = new Date();
+    cliente.cadastro = new Date();
+    this.service.salvarClientes(cliente)
+    .then(response => this.handleSuccessSave(response, cliente))
+    .catch(err => console.error(err));
+  }
+
+  handleSuccessSave(response: DocumentReference, cliente: Cliente) {
+    this.activeModal.dismiss({cliente: cliente, id: response.id, CreateMode: true})
+    ;
   }
 
   }
--- src/app/clientes/cliente/cliente.component.ts
+++ src/app/clientes/cliente/cliente.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ClienteFormComponent } from '../cliente-form/cliente-form.component';
+import { ClienteService } from 'src/app/cliente/services/cliente.service';
+import { ClienteViewModel } from 'src/app/cliente/models/cliente-view-model';
 
 @Component({
   selector: 'app-cliente',
@@ -9,9 +11,18 @@ import { ClienteFormComponent } from '../cliente-form/cliente-form.component';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor(
+    private modalService: NgbModal,
+    private service: ClienteService
+    ) { }
+
+
+  clientes: ClienteViewModel[] = [];
+  isModoInsercao: boolean = true;
+  cliente: ClienteViewModel;
 
   ngOnInit(): void {
+    this.mostrarClientes();
   }
 
   addCliente() {
@@ -25,6 +36,48 @@ export class ClienteComponent implements OnInit {
 
   handleModalClientForm(response){
     alert("Janela fechada");
+  }
+
+
+  mostrarClientes() {
+    this.service.getClientes().subscribe(response => {
+      this.clientes = [];
+      response.docs.forEach(value => {
+        const data = value.data();
+        const id = value.id;
+        const cliente: ClienteViewModel = {
+          id: id,
+          nome: data.nome,
+          endereco: data.endereco,
+          casado: data.casado,
+          modificacao: data.modificacao.toDate()
+        };
+        this.clientes.push(cliente);
+      });
+    });
+  }
+
+  deletarClick(id: string, index: string) {
+
+  }
+
+  editarClick(cliente: ClienteViewModel) {
+    const modal = this.modalService.open(ClienteFormComponent);
+    modal.result.then(
+      this.handleModalClientForm.bind(this),
+      this.handleModalClientForm.bind(this)
+    );
+    modal.componentInstance.isModoInsercao = false;
+    modal.componentInstance.cliente = cliente;
+  }
+
+  checkedCasado(index: string) {
+    const novoValor = !this.clientes[index].casado;
+    this.clientes[index].casado = novoValor;
+
+    const obj = {casado : novoValor};
+    const id = this.clientes[index].id;
+    this.service.editarClientesParcial(id, obj);
   }
 
 }
