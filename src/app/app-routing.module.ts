eb7c5932a91ce0f9c15d71782d1948e51b7eba9b
Alexander Massuda <alexcaiom@gmail.com>
2020-04-27 22:53:10 -0300

    Roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClienteComponent } from './clientes/cliente/cliente.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
